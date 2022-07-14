3ba8bf317ff2d66f58ce6ff3694aa33d0be2ba25
manjit <manjitsingh.bits@gmail.com>
2022-07-13 20:16:26 -0700

    Add text box and submit button

--- src/App.js
+++ src/App.js
@@ -5,8 +5,48 @@ import Category from './category/Category';
 import Cards from './cards/Cards';
 import Suggestions from './suggestions/Suggestions';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Col, Row} from 'react-bootstrap';
 
 class App extends React.Component {
+
+ constructor(props) {
+    super(props);
+    this.state = {"cards": []};
+
+  }
+
+componentWillMount() {
+      fetch("https://api-dev.frugalpants.com/banks/cards")
+        .then(res => res.json())
+        .then(
+          (result) => {
+            this.setState({
+              isLoaded: true,
+              rawCards: result.response
+            });
+            this.buildCardItems(result.response)
+          },
+          // Note: it's important to handle errors here
+          // instead of a catch() block so that we don't swallow
+          // exceptions from actual bugs in components.
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error
+            });
+          }
+        )
+    }
+
+    buildCardItems(response){
+    var json = response;
+    json.map((cardDetails, i ) => {
+        var obj = [];
+        obj["value"] = cardDetails.cardName;
+        obj["label"] = (<Row><Col><img className="rounded float-left img-fluid" src={cardDetails.cardImageUrl}/></Col><Col className="text-center">{cardDetails.cardName}</Col></Row>);
+        this.state.cards.push(obj);
+      })
+    }
   render() {
     return (
         <div className="App">
@@ -16,7 +56,7 @@ class App extends React.Component {
                     <Category> </Category>
                 </div>
                 <div>
-                    <Cards> </Cards>
+                    <Cards cards={this.state.cards}> </Cards>
                 </div>
                 <div>
                     <Suggestions></Suggestions>
--- src/cards/Cards.js
+++ src/cards/Cards.js
@@ -1,55 +1,14 @@
 import './Cards.css';
 import React from 'react';
 import Select from 'react-select';
-import {Col, Row} from 'react-bootstrap';
+
 
 class Cards extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {"cards": []};
-
-  }
-
-  componentWillMount() {
-      fetch("https://api-dev.frugalpants.com/banks/cards")
-        .then(res => res.json())
-        .then(
-          (result) => {
-            this.setState({
-              isLoaded: true,
-              rawCards: result.response
-            });
-            this.buildCardItems(result.response)
-          },
-          // Note: it's important to handle errors here
-          // instead of a catch() block so that we don't swallow
-          // exceptions from actual bugs in components.
-          (error) => {
-            this.setState({
-              isLoaded: true,
-              error
-            });
-          }
-        )
-
-    }
-
-    buildCardItems(response){
-    var json = response;
-    json.map((cardDetails, i ) => {
-        var obj = [];
-        obj["value"] = cardDetails.cardName;
-        obj["label"] = (<Row><Col><img className="rounded float-left img-fluid" src={cardDetails.cardImageUrl}/></Col><Col className="text-center">{cardDetails.cardName}</Col></Row>);
-        this.state.cards.push(obj);
-      })
-    }
-
-
 
   render() {
     return (
         <div className="Cards">
-            <Select options={this.state.cards}  isMulti name="colors" className="basic-multi-select" placeholder="Cards I Own" classNamePrefix="select" > </Select>
+            <Select options={this.props.cards}  isMulti name="colors" className="basic-multi-select" placeholder="Cards I Own" classNamePrefix="select" > </Select>
         </div>
     );
   }

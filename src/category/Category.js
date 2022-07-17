2ad5d270f189f95272107e973934533e68ac972e
manjit <manjitsingh.bits@gmail.com>
2022-07-16 21:17:04 -0700

    Add text box and submit button

---
+++ src/category/Category.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Select from 'react-select';
+
+class Category extends React.Component {
+  constructor(props) {
+    super(props);
+    this.categories = {"categories": [{value:'GROCERY',label:"Grocery"},
+                    {value:'GAS',label:"Gas"},
+                    {value:'PHARMACY',label:"Pharmacy"},
+                    {value:'OTHER',label:"Other"},
+                    {value:'WHOLESALE',label:"Wholesale"},
+                    {value:'HOTEL',label:"Hotel"},
+                    {value:'CARS',label:"Cars"},
+                    {value:'RESTAURANT',label:"Restaurant"}]};
+  }
+  render() {
+    return (
+        <>
+            <Select options={this.categories.categories} placeholder="I am shopping for"
+                                                        isMulti
+                                                        name="colors"
+                                                        className="basic-multi-select"
+                                                        classNamePrefix="select" />
+        </>
+    );
+  }
+}
+export default Category;

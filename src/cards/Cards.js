405e5fe87efaae8f7fb391521c89185cbe129bce
manjit <manjitsingh.bits@gmail.com>
2022-07-17 22:05:09 -0700

    Add text box and submit button

--- src/cards/Cards.js
+++ src/cards/Cards.js
@@ -5,9 +5,9 @@ class Cards extends React.Component {
 
   render() {
     return (
-        <div className="Cards">
+        <>
             <Select options={this.props.cards}  isMulti name="colors" className="basic-multi-select" placeholder="Cards I Own" classNamePrefix="select" > </Select>
-        </div>
+        </>
     );
   }
 }
